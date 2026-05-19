5b4d389abc76cc0622c1889a6e6387db37daba07
Kavesna Nair <kavesnanair1702@gmail.com>
2026-05-19 11:17:01 +0800

    addding more craving

--- project.py
+++ project.py
@@ -5,11 +5,16 @@ app = Flask(__name__)
 
 # Food database
 foods = [
-    {"category": "India Food", "name": "Banana Leaf", "price": 18},
+
+    {"category": "Indian Food", "name": "Banana Leaf", "price": 18},
     {"category": "Bevereges", "name": "Coffee", "price":20},
     {"category": "Mamak", "name": "mee goreng", "price": 3},
-    {"category": "Western", "name": "Chickcen Chop", "price": 5},
-    {"category": "Western", "name": "Mac and Cheese", "price": 12}
+    {"category": "Western Food", "name": "Chickcen Chop", "price": 5},
+    {"category": "Western Food", "name": "Mac and Cheese", "price": 12},
+    {"category": "Western Food", "name": "Lamb Chop", "price": 15},
+    {"category": "Malay Food", "name": "Laksa", "price": 8},
+    {"category": "Malay Food", "name": "Nasi Lemak", "price": 6},
+    {"category": "Indonesia Food", "name": "Ayam Gempuk", "price": 11.90}
 ]
 
 @app.route("/", methods=["GET", "POST"])
@@ -37,10 +42,9 @@ def index():
             message = "No matching food. Try different options!"
     
     return render_template(
-        "index.html",
-        recommendations=recommendations,
-        message=message
-
+         "index.html",
+         recommendation=recommendation,
+         message=message
     )
 
 if __name__ == "_main_":
